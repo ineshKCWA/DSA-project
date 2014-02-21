d31034428003066b9c70dba7aa89cfcfd000c9c0
ineshKCWA <Imesha@192.168.1.2>
2014-02-22 00:13:40 +0530

    still try to understand

--- src/test2.java
+++ src/test2.java
@@ -4,6 +4,7 @@ public class test2 {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		System.out.println("this is the new comment");
+		System.out.println("still cant figure it out");
 	}
 
 }
